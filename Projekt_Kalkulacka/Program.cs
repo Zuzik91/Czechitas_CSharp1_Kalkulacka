99f8de9730324a5028f29378db58a53f33f5e988
Zuzik91 <zuzana.smidova91@seznam.cz>
2024-04-16 18:24:31 +0200

    Kalkulacka_V1

---
+++ Projekt_Kalkulacka/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Kalkulacka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Zadej první číslo:");
+            double prvniCislo = double.Parse(Console.ReadLine());
+
+            Console.Write("Zadej operátor (+, -, *, /):");
+            string oper = Console.ReadLine();
+
+            Console.Write("Zadej druhé číslo:");
+            double druheCislo = double.Parse(Console.ReadLine());
+
+            double result = 0;
+            if (oper == "+")
+            {
+                result = prvniCislo + druheCislo;
+            }
+            else if (oper == "-")
+            {
+                result = prvniCislo - druheCislo;
+            }
+            else if (oper == "*")
+            {
+                result = prvniCislo * druheCislo;
+            }
+            else if (oper == "/")
+            {
+                result = prvniCislo / druheCislo;
+            }
+            Console.WriteLine("Výsledek: " + prvniCislo + oper + druheCislo + "=" + result);
+        }
+    }
+}
